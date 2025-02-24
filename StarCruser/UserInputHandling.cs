fe8868d1cb4513bbf00cf22c9e41405723d41a62
thoma <thoma@TJ-LAPTOP-GAME>
2025-02-24 11:57:56 +0200

    Inputs temp stored in HeshSet

--- StarCruser/UserInputHandling.cs
+++ StarCruser/UserInputHandling.cs
@@ -7,34 +7,38 @@
 
         while (Console.KeyAvailable) // Check for multiple key inputs
         {
+            HashSet<ConsoleKey> detectedKey = new HashSet<ConsoleKey>();
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             ConsoleKey key = keyInfo.Key;
+            detectedKey.Add(key);
             bool isShiftPressed = (keyInfo.Modifiers & ConsoleModifiers.Shift) != 0;
             int range = isShiftPressed ? 5 : 1;
-            if(Program.player.GetPosX() - range < 0 || Program.player.GetPosX() + range > windowSizeX - 3)
+            if (Program.player.GetPosX() - range < 0 || Program.player.GetPosX() + range > windowSizeX - 3)
             {
                 range = 1;
             }
-           
+
             // Handle movement
-            if (key == ConsoleKey.LeftArrow && Program.player.GetPosX() > 1)
+            if (detectedKey.Contains(ConsoleKey.LeftArrow) && Program.player.GetPosX() > 1)
             {
                 Draw.SetCursorAndDraw(Program.player.GetPosX(), windowSizeY - 1, "  ");
                 Program.player.SetPosX(Program.player.GetPosX() - range);
             }
-            if (key == ConsoleKey.RightArrow && Program.player.GetPosX() < windowSizeX - 3)
+            if (detectedKey.Contains(ConsoleKey.RightArrow) && Program.player.GetPosX() < windowSizeX - 3)
             {
                 Draw.SetCursorAndDraw(Program.player.GetPosX(), windowSizeY - 1, "  ");
                 Program.player.SetPosX(Program.player.GetPosX() + range);
             }
 
             // Handle shooting (Spacebar or Shift + Arrow)
-            if (key == ConsoleKey.Spacebar)
+            if (detectedKey.Contains(ConsoleKey.Spacebar))
             {
-                GameObject p = GameObject.CreateNewObject(Grafix.bullet, Program.player.GetPosX(), Program.player.GetPosY() - 1,1,true);
+                GameObject p = GameObject.CreateNewObject(Grafix.bullet, Program.player.GetPosX(), Program.player.GetPosY() - 1, 1, true);
                 Program.projectils.Add(p);
             }
         }
+
     }
 }
+
 
